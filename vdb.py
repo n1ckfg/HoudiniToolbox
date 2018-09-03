12d574020f8ac8265a38e23f41f475c9b9aa7e5c
n1ckfg <nick@fox-gieg.com>
2018-09-02 23:46:45 -0400

    open file method

--- vdb.py
+++ vdb.py
@@ -14,6 +14,9 @@ def remap(value, min1, max1, min2, max2):
 def remapInt(value, min1, max1, min2, max2):
     return int(interp(value,[min1, max1],[min2, max2]))
 
+def openFileDialog():
+    return hou.ui.selectFile(start_directory=None, title=None, collapse_sequences=False, file_type=hou.fileType.Any, pattern="*.latk, *.json", default_value=None, multiple_select=False, image_chooser=False, chooser_mode=hou.fileChooserMode.Read)
+
 url = "C:/Users/nick/Desktop/flower_testbed/flower.json"
 with open(url) as data_file:
     data = json.load(data_file)
