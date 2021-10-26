b95c4d4b88a5d5bbc41e115127e20b5f84036c2b
Nick Fox-Gieg <nick@fox-gieg.com>
2021-10-25 21:59:14 -0400

    Update write.py

--- write.py
+++ write.py
@@ -5,18 +5,6 @@ node = hou.pwd()
 geo = node.geometry()
 time = hou.frame() / hou.fps()
 
-def alert(msg):
-    hou.ui.displayMessage(str(msg))
-
-def remap(value, min1, max1, min2, max2):
-    return interp(value,[min1, max1],[min2, max2])
-
-def remapInt(value, min1, max1, min2, max2):
-    return int(interp(value,[min1, max1],[min2, max2]))
-
-def openFileDialog():
-    return hou.ui.selectFile(start_directory=None, title=None, collapse_sequences=False, file_type=hou.fileType.Any, pattern="*.latk, *.json", default_value=None, multiple_select=False, image_chooser=False, chooser_mode=hou.fileChooserMode.Read)
-
 def cacheJson(url):
     data = None
     if "jsonLoaded" in hou.expressionGlobals():
@@ -43,20 +31,3 @@ for layer in data["grease_pencil"][0]["layers"]:
                     point.setPosition(hou.Vector3(co[0], co[1], co[2]))
                     poly.addVertex(point);
 
-#~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
-
-node = hou.pwd()
-geo = node.geometry()
-
-pt0 = geo.createPoint()
-pt0.setPosition(hou.Vector3(1, 0, 0))
-pt1 = geo.createPoint()
-pt1.setPosition(hou.Vector3(0, 1, 0))
-pt2 = geo.createPoint()
-pt2.setPosition(hou.Vector3(0, 0, 1))
-
-poly = geo.createPolygon()
-poly.setIsClosed(0)
-poly.addVertex(pt0);
-poly.addVertex(pt1);
-poly.addVertex(pt2);
